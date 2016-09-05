9fd07c9e5622dff638c84304e8d50480838a981a
seethaa <seetha.annamraju@gmail.com>
2016-09-04 23:25:25 -0700

    some UI changes to preview screen, fixed timepicker military format

--- app/src/main/java/fragments/TimePickerDialogFragment.java
+++ app/src/main/java/fragments/TimePickerDialogFragment.java
@@ -22,14 +22,31 @@ public class TimePickerDialogFragment extends DialogFragment
         int hour = c.get(Calendar.HOUR_OF_DAY);
         int minute = c.get(Calendar.MINUTE);
 
+
         // Create a new instance of TimePickerDialog and return it
         return new TimePickerDialog(getActivity(), this, hour, minute,
                 DateFormat.is24HourFormat(getActivity()));
+
     }
 
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+        String am_pm = "";
+
+        Calendar datetime = Calendar.getInstance();
+        datetime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        datetime.set(Calendar.MINUTE, minute);
+
+        if (datetime.get(Calendar.AM_PM) == Calendar.AM)
+            am_pm = "AM";
+        else if (datetime.get(Calendar.AM_PM) == Calendar.PM)
+            am_pm = "PM";
+
+        String strHrsToShow = (datetime.get(Calendar.HOUR) == 0) ?"12":datetime.get(Calendar.HOUR)+"";
+
+
         // Do something with the time chosen by the user
         Button etTime= (Button) getActivity().findViewById(R.id.etTime);
-        etTime.setText(hourOfDay+":"+minute);
+        etTime.setText(strHrsToShow+":"+datetime.get(Calendar.MINUTE)+" "+am_pm);
     }
 }
