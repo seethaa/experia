e415c1e0dd874d2a4bf1f336e2c7c279f993e098
Yu-Shiuan Chu <mz0401sam.ee96@gmail.com>
2016-09-06 13:38:46 -0700

    Hard code date to set to Today, and remove date picker

--- app/src/main/java/fragments/DatePickerDialogFragment.java
+++ app/src/main/java/fragments/DatePickerDialogFragment.java
@@ -19,7 +19,7 @@ public class DatePickerDialogFragment extends DialogFragment
         // Use the current date as the default date in the picker
         final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
-        int month = c.get(Calendar.MONTH);
+        int month = c.get(Calendar.MONTH)+1;
         int day = c.get(Calendar.DAY_OF_MONTH);
 
         // Create a new instance of DatePickerDialog and return it
@@ -29,7 +29,7 @@ public class DatePickerDialogFragment extends DialogFragment
     public void onDateSet(DatePicker view, int year, int month, int day) {
         // Do something with the date chosen by the user
         Button etDate = (Button) getActivity().findViewById(R.id.etDate);
-        String Month = (month < 10)? "0" + Integer.toString(month+1) : Integer.toString(month+1);
+        String Month = (month < 10)? "0" + Integer.toString(month) : Integer.toString(month);
         String Day = (day < 10)? "0" + Integer.toString(day) : Integer.toString(day);
         String Year = Integer.toString(year);
         etDate.setText(Month+"/"+Day+"/"+Year);
